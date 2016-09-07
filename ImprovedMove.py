bd717559d6fbda6258d8d8c7cd3b8688236f04a8
Wesley Andrade <wafermonster@yahoo.com>
2016-09-07 12:42:11 -0500

    Dimension Filter

--- ImprovedMove.py
+++ ImprovedMove.py
@@ -1,7 +1,14 @@
 #replace with src and dst
 
 import os, shutil
-for root, dirs, files in os.walk('/Users/Weezy/Desktop/Songs'):
+from PIL import Image
+
+for root, dirs, files in os.walk('C:\Users\Wesley\AppData\Local\osu!\Songs'):
     for file in files:
     	if file[-4:].lower() == '.jpg':
-    		shutil.copy(os.path.join(root, file), os.path.join('/Users/Weezy/Desktop/b', file))
+    		with Image.open(file) as im:
+    			width, height = im.size
+    			if width >= 1280 && height >= 720:
+    				shutil.copy(os.path.join(root, file), os.path.join('C:\Users\Wesley\Desktop\walls', file))
+
+#walks through osu folder and only add wallpapers that are above the 1280x720 dimension range
